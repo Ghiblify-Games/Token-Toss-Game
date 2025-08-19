9ac2cea8a41bae92f540d8d2a727248c299f3c85
GhiblifyGames <ghiblifygames@gmail.com>
2025-08-19 02:53:06 -0500

    v1.32

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "Ghiblify Games-Token Toss-1.0.31";
+const cacheName = "Ghiblify Games-Token Toss-1.0.32";
 const contentToCache = [
     "Build/WebGL.loader.js",
     "Build/WebGL.framework.js",
